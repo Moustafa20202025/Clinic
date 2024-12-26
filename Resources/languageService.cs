bb494803c6c86bbd16da9fa474299163ac45ddb6
Moustafa Saeed <Mustafa.AbdelGafar@outlook.com>
2024-12-26 13:53:53 +0200

    Add files via upload

---
+++ Resources/languageService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+
+namespace ClinicProject.Resources
+{
+    public class languageService
+    {
+        private readonly IStringLocalizer _stringLocalizer;
+        public languageService(IStringLocalizerFactory factory)
+        {
+            var type = typeof(SharedResources);
+            var assmblyname = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _stringLocalizer = factory.Create("SharedResources", assmblyname.Name);
+
+        }
+        public LocalizedString GetLocalizedHTML(string key)
+        {
+            return _stringLocalizer[key];
+        }
+    }
+}
